f805a651f3e4cf4597ecea821b4551b0aee67c40
janet <janetngei18@gmail.com>
2023-02-13 17:16:43 +0300

    videocard updates

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,30 +1,39 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect} from "react";
 import './VideoCard.css';
 import VideoHeader from "./VideoHeader";
 // import VideoFooter from "./VideoFooter";
 
 const VideoCard = ({url, channel, likes, shares, avatarSrc, song}) => {
-    const [playing, setPlaying] = useState('false');
+    const [playing, setPlaying] = useState(false);
     const videoRef = useRef(null);
 
     const handleVideoPress = () => {
         if(playing) {
             videoRef.current.pause();
-            setPlaying('false');
+            setPlaying(false);
         } else {
             videoRef.current.play();
-            setPlaying('true');
+            setPlaying(true);
         }
         
-    }
+    };
+
+    useEffect(() => {
+       const scroll = document.getElementByIdEventListener("");
+       if (scroll) {
+        scroll.addEventListener("scroll", () => {
+          videoRef.current.pause();
+        });
+      }
+
+    }, [])
   return (
     <div className="videoCard">
         <VideoHeader/>
-        <video width="" height="" controls
+        <video autoPLay loop width="" height="" controls
         src={url}
         className="videoCard__player"
         alt="Short Video App"
-        loop 
         ref={videoRef}
         onClick={handleVideoPress}>
         </video>
--- src/components/VideoHeader.js
+++ src/components/VideoHeader.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './VideoHeader.css';
-import CameraAltOutlinedIcon from '@mui/icons-material/CameraAltOutlined';
-import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutlined';
+import { CameraAltIcon } from '@mui/icons-material';
+import { ArrowBackIosIcon }from '@mui/icons-material';
 
 const VideoHeader = () => {
   return (
     <div className='videoHeader'>
-        <ArrowBackIosNewOutlinedIcon/>
-        <h3>Shorts</h3>
-        <CameraAltOutlinedIcon/>
+        <ArrowBackIosIcon/>
+        <h3>Reels</h3>
+        <CameraAltIcon/>
 
     </div>
   )
